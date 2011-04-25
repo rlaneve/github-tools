d49a1818b6bf38553c5e8be27c9127fd8aed32cd
Ryan LaNeve <ryan.laneve@gmail.com>
2011-04-25 11:50:22 -0400

    Adding missing AssemblyInfo.cs file.
    Updating .gitignore to stop ignoring AssemblyInfo.cs.

---
+++ src/hooks/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GithubTools.Hooks")]
+[assembly: AssemblyDescription("Hooks for GitHub")]
+[assembly: AssemblyCompany("Ryan LaNeve")]
+[assembly: AssemblyProduct("GitHubTools - Hooks")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
