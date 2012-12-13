cef8a580f29d1f269705c9505897de3a4c3ee4b2
Ryan LaNeve <ryan.laneve@aciss.com>
2012-12-13 14:50:02 -0500

    Adds powershell-based build script.

---
+++ default.ps1
@@ -0,0 +1,48 @@
+Properties {
+    $rootDir = Split-Path $psake.build_script_file    
+    $slnPath = "$rootDir\src\github-tools.sln"
+    $outDir = "$rootDir\build\"
+}
+
+TaskSetup {
+    TeamCity-ReportBuildProgress "Running task $($psake.context.Peek().currentTaskName)"
+}
+
+task default -depends set-version,rebuild,copy-output
+
+task rebuild -depends clean,build
+
+task set-version {
+    $gittag = (& git describe --long)
+    if ($gittag -match '-(\d+)-') {
+        $gitnumber = $matches[1]
+    } else {
+        $gitnumber = 0
+    }
+    $tc_build_number = "$env:BUILD_NUMBER-$gitnumber"
+    TeamCity-SetBuildNumber $tc_build_number
+}
+
+task clean {
+    Write-Host "Creating build output directory" -ForegroundColor Green
+    if (Test-Path $outDir) {
+        rd $outDir -rec -force | out-null
+    }
+    mkdir $outDir | out-null
+
+    Write-Host "Cleaning solution" -ForegroundColor Green
+    exec { msbuild $slnPath '/t:Clean' '/v:quiet' }
+}
+
+task build { 
+    exec { msbuild "$slnPath" /t:Build /v:minimal }
+}
+
+task copy-output {
+    mkdir $outDir\hooks | out-null
+    Copy-Item .\src\hooks\Web.config $outDir\hooks\Web.config
+    Copy-Item .\src\hooks\bin $outDir\hooks -recurse
+    Copy-Item .\src\hooks\Views $outDir\hooks -recurse
+
+    TeamCity-PublishArtifact $outDir
+}
